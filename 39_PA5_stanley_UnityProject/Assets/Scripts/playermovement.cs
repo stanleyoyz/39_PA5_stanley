b7977901b1e8f6a1558a00b88a299e19694b5b7b
Tingwei-hub <64124325+Tingwei-hub@users.noreply.github.com>
2020-05-08 11:51:35 +0800

    Updated by Lau Ting Wei

---
+++ 39_PA5_stanley_UnityProject/Assets/Scripts/playermovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class playermovement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody PlayerRigidbody;
+  public float score = 0;
+    public Text coincollect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        PlayerRigidbody.AddForce(movement * speed * Time.deltaTime);
+        
+        if (score >= 8)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+        coincollect.text = "Coins collected: " + score;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "coin")
+        {
+            Destroy(other.gameObject);
+            score += 1;
+            
+        }
+        if (other.gameObject.tag == "obstacle")
+        {
+            SceneManager.LoadScene("gamelose");
+        }
+        if (score == 4)
+        {
+            SceneManager.LoadScene("Gameplay_Level2");
+        }
+    }
+   
+}
